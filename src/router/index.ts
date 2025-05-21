cb68905e7bdd03d65298239b8a81653950908b0a
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-05-20 22:36:21 -0500

    se sube frontend5

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
+//@ts-ignore
 import Writers from '../components/Writers.vue';
+//@ts-ignore
 import Books from '../components/Books.vue';
 
 const routes = [
